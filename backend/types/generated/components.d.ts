1cb39e92e7310a6c03435ffc1aa25687d4c73e5b
ratishjain <ratishjain6@gmail.com>
2024-09-15 14:22:42 +0530

    auth in progress

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,92 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    Heading: Attribute.String;
+    Description: Attribute.Text;
+    Image: Attribute.Media<'images'>;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+    description: '';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctaButton: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+    description: '';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.Text;
+    socialLink: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface LayoutFeatureSection extends Schema.Component {
+  collectionName: 'components_layout_feature_sections';
+  info: {
+    displayName: 'Feature Section';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    feature: Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+    description: '';
+  };
+  attributes: {
+    url: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+    text: Attribute.Text;
+  };
+}
+
+export interface ComponentsFeature extends Schema.Component {
+  collectionName: 'components_components_feature_s';
+  info: {
+    displayName: 'Feature ';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subheading: Attribute.Text;
+    icon: Attribute.Enumeration<['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'layout.hero-section': LayoutHeroSection;
+      'layout.header': LayoutHeader;
+      'layout.footer': LayoutFooter;
+      'layout.feature-section': LayoutFeatureSection;
+      'components.link': ComponentsLink;
+      'components.feature': ComponentsFeature;
+    }
+  }
+}
